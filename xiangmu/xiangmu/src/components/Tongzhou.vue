5531f01d433f27a5aa3e0c12439385a44c4f7a69
666github <914559666@qq.com>
2020-01-15 11:10:19 +0800

    esri-loader

--- xiangmu/xiangmu/src/components/Tongzhou.vue
+++ xiangmu/xiangmu/src/components/Tongzhou.vue
@@ -1,119 +1,109 @@
 <template>
     <div>
-        <el-container style=" border: 1px solid #eee">                    
-              
-		      <el-drawer
-		        title="标题"
-		        :direction="direction"
-		        :visible.sync="drawer"
-		        :modal="false"
-		        size=300px
-		        >
-		        <span>我来啦!</span>
-		      </el-drawer>
-			  <el-container>
-			    <el-header style="text-align: right; font-size: 12px">   
-			        <!-- 下拉 -->
-	                <el-dropdown>
-	                  <i class="el-icon-setting" style="margin-right: 15px"></i>
-	                  <el-dropdown-menu slot="dropdown">
-	                    <el-dropdown-item>个人信息</el-dropdown-item>
-	                    <el-dropdown-item>密码修改</el-dropdown-item>
-	                    <el-dropdown-item>退出</el-dropdown-item>
-	                  </el-dropdown-menu>
-	                </el-dropdown>                  
-	                <span>已登录</span>
-	                </el-header>
-			   			 <el-main>
-			              	<el-container>
-							    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-					              	<div style="border: solid 1px red;border-radius: 5px 5px 0 0;">
-					              		<p style="text-align: center;height: 25px;margin: 5px 0;">菜单</p>
-					              		<el-tree
-									      :data="datalist"
-									      show-checkbox
-									      node-key="id"
-									      ref="tree"
-									      highlight-current
-									      :default-checked-keys="[1,4]"
-									      :props="defaultProps">
-									  	</el-tree>
-									  	<el-tree
-									      :data="datalist2"
-										  node-key="id" 
-										  show-checkbox
-										  ref="tree"
-										  @check-change="handleCheckChange"
-									      :default-checked-keys="[72]"
-									      :props="defaultProps">
-									  	</el-tree>
-					              	</div> 
-					            </el-aside>
-						    	<el-main style="padding-top: 0;">   
-						    		<el-row :gutter="20">
-									  <el-col :span="8"><div class="grid-content bg-purple">						  	
-									  	<el-select v-model="value" placeholder="图层选择" style="width: 100px;">
-										    <el-option
-										      v-for="item in options"
-										      :key="item.value"
-										      :label="item.label"
-										      :value="item.value">
-										    </el-option>
-									    </el-select>
-									    <el-select v-model="valueJie" placeholder="街乡镇" style="width: 100px;">
-										    <el-option
-										      v-for="item in options"
-										      :key="item.value"
-										      :label="item.label"
-										      :value="item.value">
-										    </el-option>
-									    </el-select>
-									    <el-select v-model="valueCun" placeholder="村社区" style="width: 100px;">
-										    <el-option
-										      v-for="item in options"
-										      :key="item.value"
-										      :label="item.label"
-										      :value="item.value">
-										    </el-option>
-									    </el-select>
-									  </div></el-col>
-									  <el-col :span="6"><div class="grid-content bg-purple">
-									  	<el-select v-model="value1" placeholder="图层选择" style="width: 150px;">
-										    <el-option
-										      v-for="item in options"
-										      :key="item.value"
-										      :label="item.label"
-										      :value="item.value">
-										    </el-option>
-									    </el-select>
-									    <el-input v-model="input" placeholder="请输入id" prefix-icon="el-icon-search" style="width: 100px;"></el-input>
-									  </div></el-col>
-									  <el-col :span="10"><div class="grid-content bg-purple"><TongzhouEdit style="float: right;"/></div></el-col>
-									</el-row>
-			              		</el-main>
-						  </el-container>
-						  <el-button @click="drawer = true" type="primary" style="margin-left: 16px;"> 左侧打开 </el-button> 
-		              </el-main>
-			    		  <el-footer>Footer</el-footer>
-			  </el-container>             			  		  
+        <el-container style=" border: 1px solid #eee">                                 
+		        <el-aside v-show="drawer">
+		      		<el-drawer
+				        title="查询结果"
+				        :direction="direction"
+				        :visible.sync="drawer"
+				        :modal="false"
+				        size=300px
+				        :wrapperClosable="false">
+				        <span>暂无内容</span>
+		        	</el-drawer>
+		        </el-aside>
+			    <el-container>
+				    <el-header style="text-align: right; font-size: 12px">   
+				        <!-- 下拉 -->
+		                <el-dropdown>
+		                  <i class="el-icon-setting" style="margin-right: 15px"></i>
+		                  <el-dropdown-menu slot="dropdown">
+		                    <el-dropdown-item>个人信息</el-dropdown-item>
+		                    <el-dropdown-item>密码修改</el-dropdown-item>
+		                    <el-dropdown-item>退出</el-dropdown-item>
+		                  </el-dropdown-menu>
+		                </el-dropdown>                  
+		                <span>已登录</span>
+		            </el-header>
+			   		<el-main class="mapContent" id="map">
+		              	<el-container>
+						    <el-aside width="200px" style="background-color: rgb(238, 241, 246);">
+				              	<div style="border: solid 1px red;border-radius: 5px 5px 0 0;">
+				              		<p style="text-align: center;height: 25px;margin: 5px 0;">菜单</p>
+				              		<el-tree
+								      :data="datalist"
+								      show-checkbox
+								      node-key="id"
+								      ref="tree"
+								      highlight-current
+								      :default-checked-keys="[1,4]"
+								      :props="defaultProps">
+								  	</el-tree>
+								  	<el-tree
+								      :data="datalist2"
+									  node-key="id" 
+									  show-checkbox
+									  ref="tree"
+									  @check-change="handleCheckChange"
+								      :default-checked-keys="[72]"
+								      :props="defaultProps">
+								  	</el-tree>
+				              	</div> 
+				            </el-aside>
+					    	<el-main style="padding-top: 0;">   
+					    		<el-row :gutter="20">
+								  <el-col :span="8"><div class="grid-content bg-purple">						  	
+								  	<el-select v-model="value" placeholder="图层选择" style="width: 100px;">
+									    <el-option
+									      v-for="item in options"
+									      :key="item.value"
+									      :label="item.label"
+									      :value="item.value">
+									    </el-option>
+								    </el-select>
+								    <el-select v-model="valueJie" placeholder="街乡镇" style="width: 100px;">
+									    <el-option
+									      v-for="item in options"
+									      :key="item.value"
+									      :label="item.label"
+									      :value="item.value">
+									    </el-option>
+								    </el-select>
+								    <el-select v-model="valueCun" placeholder="村社区" style="width: 100px;">
+									    <el-option
+									      v-for="item in options"
+									      :key="item.value"
+									      :label="item.label"
+									      :value="item.value">
+									    </el-option>
+								    </el-select>
+								  </div></el-col>
+								  <el-col :span="6"><div class="grid-content bg-purple">
+								  	<el-select v-model="value1" placeholder="图层选择" style="width: 150px;">
+									    <el-option
+									      v-for="item in options"
+									      :key="item.value"
+									      :label="item.label"
+									      :value="item.value">
+									    </el-option>
+								    </el-select>
+								    <el-input v-model="input" placeholder="请输入id" prefix-icon="el-icon-search" style="width: 100px;"></el-input>
+								  </div></el-col>
+								  <el-col :span="10"><div class="grid-content bg-purple"><TongzhouEdit style="float: right;"/></div></el-col>
+								</el-row>
+		              		</el-main>
+					    </el-container>
+					    <el-button @click="drawer = true" type="primary" style="margin-left: 16px;"> 左侧打开 </el-button> 
+		            </el-main>
+			        <el-footer>Footer</el-footer>
+			</el-container>             			  		  
         </el-container>   
     </div>
 </template>
 <script>
   //tooltip 内不支持 router-link 组件，请使用 vm.$router.push 代替。
+import esriLoader from 'esri-loader';
 import TongzhouEdit from './TongzhouEdit.vue';
-// Vue.component('aSide', {
-//          template:`
-//              <slot name='true'/>
-//          `
-//      })
-//var Aside={
-//	template:`
-//		<aSide>
-//			<aside slot=${drawer}></aside>
-//		</aSide>
-//	`
-//}
 export default {
     name:'Tongzhou',
     data() {
@@ -150,7 +140,6 @@ export default {
             fuheinput:'',
             select:'',
             restaurants: [],
-            drawer: false,
             dialogVisible: false,
             // fits:[{fitImg:'fill',url:imgs,title:'fill'}]
             count: 0,
@@ -284,6 +273,75 @@ export default {
     },
     created() {
       this.countsImg=['s','r','geag','sdfsdfds','s','r','geag','sdfsdfds','s','r','geag','sdfsdfds','s','r','geag','sdfsdfds','s','r','geag','sdfsdfds',]
+      var tzdz_url="http://tzfw.natapp1.cc/arcgis/rest/services/TZDZDT/MapServer";//电子地图
+	 var tzyx_url="http://tzfw.natapp1.cc/arcgis/rest/services/ImageTZ/MapServer";//影像服务	
+	var map;
+	const options = {
+  	url: 'http://localhost/arcgis_js_v327_api/arcgis_js_api/library/3.27/3.27compact/init.js' // 这里的API地址可以是官网提供的CDN，也可在此配置离线部署的地址
+	}
+	esriLoader.loadModules([
+		"esri/map",
+		"esri/layers/ArcGISDynamicMapServiceLayer",
+		"esri/undoManager",
+		"esri/tasks/GeometryService",
+		"esri/layers/GraphicsLayer","esri/geometry/Extent",
+		"esri/toolbars/draw",
+		"esri/toolbars/edit",
+		"esri/toolbars/navigation",
+		"esri/dijit/editing/Cut",
+		"esri/dijit/editing/Union",
+		"esri/graphic",
+		"esri/geometry/geometryEngine",
+		"esri/config",
+		"esri/InfoTemplate",
+		"esri/layers/FeatureLayer",
+		"esri/tasks/query",
+		"esri/Color",
+		"esri/symbols/SimpleMarkerSymbol",
+		"esri/symbols/SimpleLineSymbol",
+		"esri/symbols/SimpleFillSymbol",
+		"esri/dijit/editing/TemplatePicker",
+		"esri/renderers/SimpleRenderer",
+		"dojo/_base/connect",
+		"dojo/_base/array",
+		"dojo/_base/event",
+		"dojo/_base/lang",
+		"dojo/parser",
+		"dijit/registry",
+		"dijit/layout/BorderContainer", "dijit/layout/ContentPane",
+		"dijit/form/Button", "dojo/domReady!"], options) // 传入需要使用的类
+	.then(([Map,ArcGISDynamicMapServiceLayer, UndoManager, GeometryService,GraphicsLayer,Extent, Draw, Edit,Navigation, Cut, Union, Graphic, geometryEngine, esriConfig,InfoTemplate,
+		FeatureLayer, Query, Color,SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol,TemplatePicker,SimpleRenderer, connect,arrayUtils, event, lang, parser, registry]) => {
+	  		map = new Map("map", {			
+//			basemap: tzyx_url,
+			//去掉esri图标和缩放图标
+			logo:false,
+			slider:false,
+			//        center: [-83.244, 42.581],
+			zoom: 2
+		});
+		var Imagelayer = new esri.layers.ArcGISTiledMapServiceLayer(tzyx_url);//影像
+		var Dzdtlayer = new esri.layers.ArcGISTiledMapServiceLayer(tzdz_url);//电子
+			var featureLayerXzbj=new FeatureLayer( "http://tzfw.natapp1.cc/arcgis/rest/services/TZXZBJ/MapServer/0", {
+   			mode: FeatureLayer.MODE_SNAPSHOT,
+		    outFields: ["*"]
+		});
+		var defaultSymbolXzbj =  new SimpleFillSymbol(
+		    SimpleFillSymbol.STYLE_NULL,  // 面样式，不填充。
+		    // new SimpleLineSymbol(style, color, width)
+		    new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASH, new Color([197,97,20]), 3),
+		    new Color([197,97,20,1])  // 填充颜色。
+		);
+		var rendererXzbj = new SimpleRenderer(defaultSymbolXzbj);
+		featureLayerXzbj.setRenderer(rendererXzbj);
+		
+		map.addLayer(Imagelayer);
+		map.addLayer(Dzdtlayer);
+		map.addLayer(featureLayerXzbj);
+	})
+	.catch(err => {
+	  console.error(err)
+	})
     },
     components:{//挂子
        TongzhouEdit
@@ -293,6 +351,8 @@ export default {
 // --mode 选项参数为命令行覆写默认的模式
 </script>
 <style scoped>
+@import url('http://localhost/arcgis_js_v327_api/arcgis_js_api/library/3.27/3.27compact/dijit/themes/claro/claro.css');
+@import url('http://localhost/arcgis_js_v327_api/arcgis_js_api/library/3.27/3.27compact/esri/css/esri.css');
     span{
         color: red;
     }
